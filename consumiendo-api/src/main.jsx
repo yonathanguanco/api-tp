8d2ff8c46b711301f9616670e34db49386ca823d
Yonathan Guanco <yonathanguanco@gmail.com>
2024-06-13 12:51:55 -0300

    api

---
+++ consumiendo-api/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ComponenteApi from './Api'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ComponenteApi />
+  </React.StrictMode>,
+)
